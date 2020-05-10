1b58ee15a6a26a4a395c8893711b828973b95aa0
Michael <m@michaelti.ca>
2020-05-10 00:12:04 -0400

    Add room demo

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import SocketDemo from './SocketDemo';
 function App() {
     return (
         <div className="App">
-            <SocketDemo />
+            <SocketDemo title="Default" socketUrl="ws://localhost:3001/" />
+            <SocketDemo title="/Game" socketUrl="ws://localhost:3001/game" />
+            <SocketDemo title="/Chat" socketUrl="ws://localhost:3001/chat" />
         </div>
     );
 }
--- src/components/SocketDemo.js
+++ src/components/SocketDemo.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import useSocket from '../hooks/useSocket';
 
-function SocketDemo() {    
+function SocketDemo(props) {    
     const [messages, setMessages] = useState([]);
     const [latency, setLatency] = useState(null);
 
-    const [socket, isConnected, isConnecting] = useSocket('http://localhost:3001');
+    const [socket, isConnected, isConnecting] = useSocket(props.socketUrl);
     
     useEffect(() => {
         socket.on('pong', (latency) => {
@@ -18,7 +18,8 @@ function SocketDemo() {
     }, [socket]);
 
     return (
-        <div>
+        <div style={{float: 'left', marginRight: '50px'}}>
+            <h1>{ props.title }</h1>
             status: {
                 isConnecting ? 'Connecting' :
                 isConnected ? 'Connected' :
@@ -36,6 +37,19 @@ function SocketDemo() {
                     </div>
                 ))
             }
+            <br />
+
+            { props.title === '/Chat' ? (
+                <>
+                    <button onClick={()=>{socket.emit('join-room', 'example-room');}}>
+                        join room "example-room"
+                    </button>
+                    <button onClick={()=>{socket.emit('leave-room', 'example-room');}}>
+                        leave room "example-room"
+                    </button>
+                </>
+            ) : null }
+
         </div>
     );
 }
