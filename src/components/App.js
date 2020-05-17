8c6f742ff187ac88e67c5b5904e327ee55b32968
Michael <m@michaelti.ca>
2020-05-17 17:10:24 -0400

    Add basic styles to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import SocketActions from './SocketActions';
 import Header from './Header';
 import useSocket from '../hooks/useSocket';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr;
+`;
 
 function App() {
     const [socket, isConnected, isConnecting] = useSocket("ws://localhost:3001");
 
     return (
-        <div className="App">
+        <AppContainer className="App">
             <Header
                 socket={socket}
                 isConnecting={isConnecting}
                 isConnected={isConnected}
             />
             <SocketActions socket={socket} />
-        </div>
+        </AppContainer>
     );
 }
 
