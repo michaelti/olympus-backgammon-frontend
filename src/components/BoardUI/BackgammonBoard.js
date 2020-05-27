e481bf65ad277daef6dc544ac8504b1d2240ac46
Michael <m@michaelti.ca>
2020-05-27 16:41:11 -0400

    add xmlns attribute

--- src/components/BoardUI/BackgammonBoard.js
+++ src/components/BoardUI/BackgammonBoard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BackgammonBoard() {
     return (
-        <svg width="500" height="400" viewBox="0 0 1500 1200">
+        <svg width="500" height="400" viewBox="0 0 1500 1200" xmlns="http://www.w3.org/2000/svg">
             <defs>
                 <symbol id="pip" width="100" height="600" viewBox="0 0 100 600">
                     <polygon points="50 500 100 0 0 0 50 500" fill="#f7d086" />
