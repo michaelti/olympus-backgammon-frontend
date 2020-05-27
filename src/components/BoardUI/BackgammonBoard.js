d6851a292f1d044580355599374c4b02923c710c
Michael <m@michaelti.ca>
2020-05-27 16:14:04 -0400

    return to using svg symbol for pips

--- src/components/BoardUI/BackgammonBoard.js
+++ src/components/BoardUI/BackgammonBoard.js
@@ -1,39 +1,11 @@
 import React from 'react';
 
-const Pip = ({ oddEven="odd", ...props }) => {
-    const odd = (
-        <svg width="100" height="500" viewBox="0 0 100 500" { ...props }>
-            <polygon points="50 500 100 0 0 0 50 500" fill="red" />
-        </svg>
-    );
-
-    const even = (
-        <svg width="100" height="500" viewBox="0 0 100 500" { ...props }>
-            <polygon points="50 500 100 0 0 0 50 500" fill="red" />
-        </svg>
-    );
-
-    return (
-        oddEven === 'odd' ? odd :
-        oddEven === 'even' ? even :
-        null
-    );
-};
-
 function BackgammonBoard() {
-
-
-
     return (
         <svg width="500" height="400" viewBox="0 0 1500 1200">
             <defs>
-                <symbol id="pip--group">
-                    <Pip x="0" oddEven="odd" />
-                    <Pip x="100" oddEven="even" />
-                    <Pip x="200" oddEven="odd" />
-                    <Pip x="300" oddEven="even" />
-                    <Pip x="400" oddEven="odd" />
-                    <Pip x="500" oddEven="even" />
+                <symbol id="pip" width="100" height="600" viewBox="0 0 100 600">
+                    <polygon points="50 500 100 0 0 0 50 500" fill="red" />
                 </symbol>
             </defs>
 
@@ -45,12 +17,39 @@ function BackgammonBoard() {
             </g>
 
             <g class="pips">
-                <use href="#pip--group" transform="translate(100)" />
-                <use href="#pip--group" transform="translate(800)" />
-                <use href="#pip--group" transform="translate(100) rotate(180, 300, 600)" />
-                <use href="#pip--group" transform="translate(100) rotate(180, 650, 600)" />
+                <g class="pips--top-left">
+                    <use href="#pip" x="100" />
+                    <use href="#pip" x="200" />
+                    <use href="#pip" x="300" />
+                    <use href="#pip" x="400" />
+                    <use href="#pip" x="500" />
+                    <use href="#pip" x="600" />
+                </g>
+                <g class="pips--top-right">
+                    <use href="#pip" x="800" />
+                    <use href="#pip" x="900" />
+                    <use href="#pip" x="1000" />
+                    <use href="#pip" x="1100" />
+                    <use href="#pip" x="1200" />
+                    <use href="#pip" x="1300" />
+                </g>
+                <g class="pips--bot-right">
+                    <use href="#pip" x="100" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="200" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="300" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="400" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="500" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="600" transform="rotate(180, 750, 600)" />
+                </g>
+                <g class="pips--bot-left">
+                    <use href="#pip" x="800" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="900" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="1000" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="1100" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="1200" transform="rotate(180, 750, 600)" />
+                    <use href="#pip" x="1300" transform="rotate(180, 750, 600)" />
+                </g>
             </g>
-
         </svg>
     );
 }
