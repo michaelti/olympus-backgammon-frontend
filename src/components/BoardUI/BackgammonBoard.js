285596bc443e605f25709137eafbd93f06526cb2
Michael <m@michaelti.ca>
2020-05-28 16:16:08 -0400

    Set up to use Pip and Checker components

--- src/components/BoardUI/BackgammonBoard.js
+++ src/components/BoardUI/BackgammonBoard.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import Checker from './Checker';
 import Pip from './Pip';
 
 function BackgammonBoard() {
-    const [boardState, setBoardState] = useState({
+    const [boardState] = useState({
         pips: [
             { size: 0, top: '', bot: '' },
-            { size: 0, top: 'white', bot: 'black' },
+            { size: 3, top: 'white', bot: 'black' },
             { size: 0, top: 'white', bot: 'black' },
             { size: 0, top: 'white', bot: 'black' },
             { size: 0, top: 'white', bot: 'black' },
@@ -39,55 +38,56 @@ function BackgammonBoard() {
 
     return (
         <svg width="500" height="400" viewBox="0 0 1500 1200" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                <symbol id="pip" width="100" height="600" viewBox="0 0 100 600">
-                    <polygon points="50 500 100 0 0 0 50 500" fill="#f7d086" />
-                </symbol>
-            </defs>
+            <rect className="background" width="1500" height="1200" fill="#402d26" />
 
-            <g>
-                <rect class="background" width="1500" height="1200" fill="#402d26" />
-                <rect class="off--left" width="100" height="1200" fill="#745138" />
-                <rect class="bar" x="700" width="100" height="1200" fill="#745138" />
-                <rect class="off--right" x="1400"  width="100" height="1200" fill="#745138" />
-            
-                <g class="pips--top-left">
-                    <use width="100" height="600" href="#pip" x="100" />
-                    <use width="100" height="600" href="#pip" x="200" />
-                    <use width="100" height="600" href="#pip" x="300" />
-                    <use width="100" height="600" href="#pip" x="400" />
-                    <use width="100" height="600" href="#pip" x="500" />
-                    <use width="100" height="600" href="#pip" x="600" />
-                </g>
-                <g class="pips--top-right">
-                    <use width="100" height="600" href="#pip" x="800" />
-                    <use width="100" height="600" href="#pip" x="900" />
-                    <use width="100" height="600" href="#pip" x="1000" />
-                    <use width="100" height="600" href="#pip" x="1100" />
-                    <use width="100" height="600" href="#pip" x="1200" />
-                    <use width="100" height="600" href="#pip" x="1300" />
-                </g>
-                <g class="pips--bot-right">
-                    <use width="100" height="600" href="#pip" x="100" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="200" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="300" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="400" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="500" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="600" transform="rotate(180, 750, 600)" />
-                </g>
-                <g class="pips--bot-left">
-                    <use width="100" height="600" href="#pip" x="800" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="900" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="1000" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="1100" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="1200" transform="rotate(180, 750, 600)" />
-                    <use width="100" height="600" href="#pip" x="1300" transform="rotate(180, 750, 600)" />
-                </g>
-            </g>
+            <rect className="bar--top" x="700" width="100" height="600" fill="#745138" />
+            <rect className="bar--bot" y="600" x="700" width="100" height="600" fill="#745138" />
+            <rect className="off--left-top" width="100" height="600" fill="#745138" />
+            <rect className="off--left-bot" y="600" width="100" height="600" fill="#745138" />
+            <rect className="off--right-top" x="1400" width="100" height="600" fill="#745138" />
+            <rect className="off--right-bot" y="600" x="1400" width="100" height="600" fill="#745138" />
 
-            <Pip data={boardState.pips[1]} />
+            {boardState.pips.slice(1,7).map((pip, i) => (
+                <Pip
+                    key={i}
+                    posX={1300 - (i * 100)}
+                    invert
+                    size={pip.size}
+                    top={pip.top}
+                    bot={pip.bot}
+                />
+            ))}
 
-            <Checker posX="1" posY="11" />
+            {boardState.pips.slice(7,13).map((pip, i) => (
+                <Pip
+                    key={i}
+                    posX={600 - (i * 100)}
+                    invert
+                    size={pip.size}
+                    top={pip.top}
+                    bot={pip.bot}
+                />
+            ))}
+
+            {boardState.pips.slice(13,19).map((pip, i) => (
+                <Pip
+                    key={i}
+                    posX={100 + (i * 100)} 
+                    size={pip.size} 
+                    top={pip.top} 
+                    bot={pip.bot} 
+                />
+            ))}
+
+            {boardState.pips.slice(19,25).map((pip, i) => (
+                <Pip
+                    key={i}
+                    posX={800 + (i * 100)} 
+                    size={pip.size} 
+                    top={pip.top} 
+                    bot={pip.bot} 
+                />
+            ))}
         </svg>
     );
 }
--- src/components/BoardUI/Pip.js
+++ src/components/BoardUI/Pip.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Checker from './Checker';
 
-function Pip({ posX, posY, onClick }) {
+function Pip({ size, top, bot, posX, invert }) {
+
+    let checkers = Array(size);
+
+    if (checkers.length > 0) {
+        checkers.fill(top);
+        checkers[0] = bot;
+    }
+
     return (
         <g>
+            <svg x={posX} y={invert ? '50%' : '0'} width="100" height="600" viewBox="0 0 100 600">
+                <polygon points="50 500 100 0 0 0 50 500" fill="#f7d086" style={{ transformOrigin: '50%', transform: invert ? 'rotate(180deg)' : 'none' }} />
+                <rect height="100%" width="100%" fill="transparent"></rect>
+            </svg>
+
+            {checkers.map((checker, i) => (
+                <Checker
+                    key={i}
+                    posX={posX}
+                    posY={invert ? (1100 - (i * 100)) : (i * 100)}
+                    color={checker} />
+            ))}
         </g>
     );
 }
