bb0d7b3da282c046d2dec06a7ed7a0aeea1a28e4
Michael <m@michaelti.ca>
2020-05-28 16:39:10 -0400

    Add missing Off.js file

---
+++ src/components/BoardUI/Off.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Checker from './Checker';
+
+function Off({ count, color, posX, invert }) {
+    const checkers = Array(count).fill(color);
+
+    return (
+        <g>
+            <svg x={posX} y={invert ? '50%' : '0'} width="100" height="600" viewBox="0 0 100 600">
+                <rect width="100" height="600" fill="#745138" />
+            </svg>
+
+            {checkers.map((checker, i) => (
+                <Checker
+                    key={i}
+                    posX={posX}
+                    posY={invert ? (1100 - (i * 100)) : (i * 100)}
+                    color={checker} />
+            ))}
+        </g>
+    );
+}
+
+export default Off;
