f3a410aa2a862ebe24f01fd910f732b414889082
Michael <m@michaelti.ca>
2020-05-28 16:43:27 -0400

    get Bar component working

--- src/components/BoardUI/BackgammonBoard.js
+++ src/components/BoardUI/BackgammonBoard.js
@@ -42,8 +42,17 @@ function BackgammonBoard() {
         <svg width="500" height="400" viewBox="0 0 1500 1200" xmlns="http://www.w3.org/2000/svg">
             <rect className="background" width="1500" height="1200" fill="#402d26" />
 
-            <rect className="bar--top" x="700" width="100" height="600" fill="#745138" />
-            <rect className="bar--bot" y="600" x="700" width="100" height="600" fill="#745138" />
+            <Bar
+                posX={700}
+                invert
+                count={boardState.barWhite}
+                color="white"
+            />
+            <Bar
+                posX={700}
+                count={boardState.barBlack}
+                color="black"
+            />
             
             <Off
                 posX={1400}
--- src/components/BoardUI/Bar.js
+++ src/components/BoardUI/Bar.js
@@ -6,7 +6,17 @@ function Bar({ count, color, posX, invert }) {
 
     return (
         <g>
+            <svg x={posX} y={invert ? '50%' : '0'} width="100" height="600" viewBox="0 0 100 600">
+                <rect width="100" height="600" fill="#745138" />
+            </svg>
 
+            {checkers.map((checker, i) => (
+                <Checker
+                    key={i}
+                    posX={posX}
+                    posY={invert ? (1100 - (i * 100)) : (i * 100)}
+                    color={checker} />
+            ))}
         </g>
     );
 }
