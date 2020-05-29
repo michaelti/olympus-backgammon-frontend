520359d4f0f96855c18c1efbaca31cbaea8e3be1
Michael <m@michaelti.ca>
2020-05-29 17:54:01 -0400

    Add squishing for pips with too many checkers

--- src/components/BoardUI/Pip.js
+++ src/components/BoardUI/Pip.js
@@ -10,6 +10,9 @@ function Pip({ size, top, bot, posX, invert }) {
         checkers[0] = bot;
     }
 
+    const squishAmount = checkers.length > 6 ?
+        ((checkers.length - 6) * 100 / (checkers.length - 1)) : 0;
+
     return (
         <g>
             <svg x={posX} y={invert ? '50%' : '0'} width="100" height="600" viewBox="0 0 100 600">
@@ -21,7 +24,7 @@ function Pip({ size, top, bot, posX, invert }) {
                 <Checker
                     key={i}
                     posX={posX}
-                    posY={invert ? (1100 - (i * 100)) : (i * 100)}
+                    posY={invert ? (1100 - (i * (100 - squishAmount))) : (i * (100 - squishAmount ))}
                     color={checker} />
             ))}
         </g>
