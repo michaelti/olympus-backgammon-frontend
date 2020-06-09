5394334fa480a1644ac70f454d021e932a3b6fd9
Michael <m@michaelti.ca>
2020-06-08 23:19:04 -0400

    Add color string and getColor to player enum

--- src/util.js
+++ src/util.js
@@ -3,4 +3,12 @@ export const Player = Object.freeze({
     neither: 0,
     white:  1,
     black: -1,
+    properties: {
+        '0':  { name: 'Neither' },
+        '1':  { name: 'White' },
+        '-1': { name: 'Black' },
+    },
+    getColor(value) {
+        return this.properties[value].name;
+    },
 });
