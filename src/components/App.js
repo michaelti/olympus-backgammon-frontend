d7fcffc26c7be253986d295f28d3b4aef2ecd5bb
Michael <m@michaelti.ca>
2020-06-10 00:50:05 -0400

    Break indentation some more

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className="App">
           <GlobalStyles test='test' />
-          <SocketManager />
+                            <SocketManager />
         </div>
     );
 }
