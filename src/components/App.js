0f92a7db154c1696566008cf1a038e234fa1ab5a
Michael <m@michaelti.ca>
2020-06-24 15:12:57 -0400

    add Room wrapper component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyles from "../styles/globalStyles";
 import Header from "./Header";
 import Main from "./Main";
-import Game from "./Game";
+import Room from "./Room";
 
 function App() {
     const [roomName, setRoomName] = useState("");
@@ -15,7 +15,7 @@ function App() {
                 <Header roomName={roomName} />
                 <Switch>
                     <Route path="/room/:roomName">
-                        <Game />
+                        <Room setRoomName={setRoomName} />
                     </Route>
                     <Route path="/">
                         <Main setRoomName={setRoomName} />
