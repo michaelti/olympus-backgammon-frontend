332a9b4efee92a1ee7daccbc9f598c39b6d14690
Michael <m@michaelti.ca>
2020-07-04 18:41:45 -0400

    Add dice component with images and greying out

--- src/components/BoardUI/BackgammonExtras.js
+++ src/components/BoardUI/BackgammonExtras.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 import { Player } from "../../util";
+import Dice from "./Dice";
 
-function BackgammonExtras({ boardState, applyTurn, undoTurn }) {
+function BackgammonExtras({ boardState: { turn, dice }, applyTurn, undoTurn }) {
+    const [initialDice, setInitialDice] = useState(dice);
+    const [prevTurn, setPrevTurn] = useState(turn);
+
+    if (prevTurn !== turn) {
+        setPrevTurn(turn);
+        setInitialDice(dice);
+    }
+
     return (
         <ListGroup horizontal="lg">
-            <ListGroupItem>Turn: {Player.properties[boardState.turn].colorName}</ListGroupItem>
-            <ListGroupItem>Dice: {JSON.stringify(boardState.dice)}</ListGroupItem>
+            <ListGroupItem>Turn: {Player.properties[turn].colorName}</ListGroupItem>
+            <ListGroupItem>
+                <Dice initialDice={initialDice} remainingDice={dice} />
+            </ListGroupItem>
             <ListGroupItem>
                 <Button onClick={applyTurn} color="success">
                     Submit turn
