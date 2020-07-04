6602da108bd98f5e848c77360dba63a39fcf4660
Michael <m@michaelti.ca>
2020-07-04 18:45:55 -0400

    style

--- src/components/BoardUI/Dice.js
+++ src/components/BoardUI/Dice.js
@@ -1,11 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import Dice1 from "./svg/dice/dice-1.svg";
 import Dice2 from "./svg/dice/dice-2.svg";
 import Dice3 from "./svg/dice/dice-3.svg";
 import Dice4 from "./svg/dice/dice-4.svg";
 import Dice5 from "./svg/dice/dice-5.svg";
 import Dice6 from "./svg/dice/dice-6.svg";
-import styled from "styled-components";
 
 const Img = styled.img`
     height: 2em;
