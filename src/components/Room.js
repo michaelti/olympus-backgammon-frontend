d4c26187d6d338f9d31af39e78b8783b7e7d4a80
Michael <m@michaelti.ca>
2020-07-12 11:45:41 -0400

    RoomState enum

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, Redirect } from "react-router-dom";
 import { useSocketOn, socketEmit } from "../api";
 import { Container } from "reactstrap";
-import { Player } from "../util";
+import { Player, RoomState } from "../util";
 import { Badge } from "reactstrap";
 import RoomSetup from "./RoomSetup";
 import Game from "./Game";
@@ -35,7 +35,7 @@ function Room({ setRoomName }) {
             <Badge className="mb-3">
                 {player ? `Playing as ${Player.properties[player].colorName}` : "Spectating"}
             </Badge>
-            {player === Player.white && roomState === 1 ? <RoomSetup /> : null}
+            {player === Player.white && roomState === RoomState.setup ? <RoomSetup /> : null}
             <Game />
         </Container>
     );
