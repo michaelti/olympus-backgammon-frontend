038ec69a5a693193e15a3444bd2d837523fa45ba
Michael <m@michaelti.ca>
2020-07-11 21:42:32 -0400

    Use horizontal list group

--- src/components/RoomSetup.js
+++ src/components/RoomSetup.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { socketEmit } from "../api";
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Button,
+    ListGroup,
+    ListGroupItem,
+} from "reactstrap";
 
 function RoomSetup() {
     const [selectedVariant, setSelectedVariant] = useState(null);
@@ -19,29 +27,29 @@ function RoomSetup() {
         <Modal isOpen={isModalOpen} size="lg">
             <ModalHeader>Which game would you like to play?</ModalHeader>
             <ModalBody>
-                <Button
-                    size="lg"
-                    block
-                    onClick={() => setSelectedVariant(1)}
-                    active={selectedVariant === 1}
-                    disabled>
-                    Portes
-                </Button>
-                <Button
-                    size="lg"
-                    block
-                    onClick={() => setSelectedVariant(2)}
-                    active={selectedVariant === 2}>
-                    Plakoto
-                </Button>
-                <Button
-                    size="lg"
-                    block
-                    onClick={() => setSelectedVariant(3)}
-                    active={selectedVariant === 3}
-                    disabled>
-                    Fevga
-                </Button>
+                <ListGroup horizontal>
+                    <ListGroupItem
+                        onClick={() => setSelectedVariant(1)}
+                        active={selectedVariant === 1}
+                        tag="button"
+                        action>
+                        Portes
+                    </ListGroupItem>
+                    <ListGroupItem
+                        onClick={() => setSelectedVariant(2)}
+                        active={selectedVariant === 2}
+                        tag="button"
+                        action>
+                        Plakoto
+                    </ListGroupItem>
+                    <ListGroupItem
+                        onClick={() => setSelectedVariant(3)}
+                        active={selectedVariant === 3}
+                        tag="button"
+                        action>
+                        Fevga
+                    </ListGroupItem>
+                </ListGroup>
             </ModalBody>
             <ModalFooter>
                 <Button size="lg" color="primary" onClick={sendVariant} disabled={!selectedVariant}>
