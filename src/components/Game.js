3b29bb3682c566d032e888be92185c09d705d04e
Blaise <blaise@live.ca>
2020-08-04 19:59:03 -0400

    Add pip highlighting for fevga

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,9 +18,18 @@ function Game({ player, roomStep, startingRolls, variant, boardState }) {
 
     const getPossiblePips = (from) => {
         let possiblePips = new Set();
+        let to;
 
         for (const die of boardState.dice) {
-            const to = clamp(from + die * boardState.turn);
+            if (variant === Variant.fevga) {
+                to = from - die;
+                if (boardState.turn === Player.white) {
+                    if (from >= 13 && to <= 12) to = 25;
+                    else if (to < 1) to += 24;
+                }
+            } else {
+                to = clamp(from + die * boardState.turn);
+            }
             if (isMoveValid[variant](from, to, boardState)) {
                 possiblePips.add(to);
             }
