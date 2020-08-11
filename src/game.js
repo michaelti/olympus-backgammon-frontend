36ad555dc369d82a633e11358a3a526e540dd708
Blaise <blaise@live.ca>
2020-08-10 21:00:06 -0400

    Update move validator logic from backend

--- src/game.js
+++ src/game.js
@@ -141,7 +141,7 @@ export const isMoveValid = {
             let owned = 0;
             for (const i of homePips) if (board.pips[i].top === board.turn) owned++;
             board.pips[to].top = pipBackup; // Restore original board state
-            if (owned >= 6) return false;
+            if (owned >= 6 && board.pips[from].size > 1) return false;
 
             // Both players move in the same direction (decreasing). The exception is when we wrap
             // around the edge of the board, we jump by 24 (i.e. 3, 2, 1, 24, 23, 22)
