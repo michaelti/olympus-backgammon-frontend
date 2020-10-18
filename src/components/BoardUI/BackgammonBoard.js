bf51b3c0d22fa6b53631bbc5265b3ffb724490b8
Michael <m@michaelti.ca>
2020-10-17 23:35:41 -0400

    Add temp Pip styles and remove UI strip

--- src/components/BoardUI/BackgammonBoard.js
+++ src/components/BoardUI/BackgammonBoard.js
@@ -2,41 +2,50 @@ import React, { useState } from "react";
 import { Player } from "../../util.js";
 import styled from "styled-components";
 import CheckerStack from "./CheckerStack";
-
 const Board = styled.div`
     background: #402d26;
     width: 100%;
     height: 75vh;
     display: grid;
     grid-template-columns: repeat(15, minmax(0, 1fr));
-    grid-template-rows: minmax(0, 1fr) 50px minmax(0, 1fr);
-    grid-gap: 5px;
+    grid-template-rows: minmax(0, 1fr) minmax(0, 1fr);
+    grid-column-gap: 5px;
     grid-template-areas:
         "top-left p13 p14 p15 p16 p17 p18 top-mid p19 p20 p21 p22 p23 p24 top-right"
-        "ui ui ui ui ui ui ui ui ui ui ui ui ui ui ui"
         "bot-left p12 p11 p10 p9 p8 p7 bot-mid p6 p5 p4 p3 p2 p1 bot-right";
 `;
 
-const PipLike = styled.div`
+const BoardChild = styled.div`
     grid-area: ${(props) => props.gridArea};
-
-    cursor: ${(props) => (props.canMoveFrom || props.canMoveTo) && "pointer"};
     background-color: ${(props) => props.canMoveTo && "gray"} !important;
+    cursor: ${(props) => (props.canMoveFrom || props.canMoveTo) && "pointer"};
 `;
 
-const Pip = styled(PipLike)`
-    background-color: #f7d086;
+const Pip = styled(BoardChild)`
+    margin-top: ${(props) => props.reverse && "15px"};
+    margin-bottom: ${(props) => !props.reverse && "15px"};
+    position: relative;
+    &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 600' preserveAspectRatio='none'%3E%3Cpolygon points='50 500 100 0 0 0 50 500' fill='%23f7d086'/%3E%3C/svg%3E");
+        background-size: 100% 100%;
+        transform: ${(props) => props.reverse && "scaleY(-1)"};
+    }
 `;
 
-const Bar = styled(PipLike)`
+const Bar = styled(BoardChild)`
     background-color: #c49158;
 `;
 
-const Off = styled(PipLike)`
+const Off = styled(BoardChild)`
     background-color: #745138;
 `;
-
-const UI = styled(PipLike)``;
 
 function BackgammonBoard({
     boardState: { pips, off, turn, recentMove },
@@ -114,7 +123,8 @@ function BackgammonBoard({
                         onClick={() => handleClickPip(i)}
                         canMoveTo={highlightedPips?.has(i)}
                         canMoveFrom={isTurn && pip.top === turn && pip.size > 0}
-                        gridArea={"p" + i}>
+                        gridArea={"p" + i}
+                        reverse={i <= 12}>
                         <CheckerStack
                             size={pip.size}
                             top={pip.top}
@@ -157,8 +167,6 @@ function BackgammonBoard({
             {/* <!-- --> */}
             <Off gridArea={flipOffWhite ? "top-right" : "top-left"}></Off>
             <Off gridArea="bot-left"></Off>
-            {/* <!-- --> */}
-            <UI gridArea="ui"></UI>
         </Board>
     );
 }
