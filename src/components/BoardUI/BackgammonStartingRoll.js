5fbc3dceb3b2e4ef72655b51911fb3d92b9e649e
Michael <m@michaelti.ca>
2020-12-30 20:06:15 -0500

    Replace bootstrap button in starting roll

--- src/components/BoardUI/BackgammonStartingRoll.js
+++ src/components/BoardUI/BackgammonStartingRoll.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "reactstrap";
 import Die from "./Die";
 import { socketEmit } from "../../api";
 import styled from "styled-components";
@@ -9,6 +8,22 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     padding: 5px 0;
+`;
+
+const Button = styled.button`
+    font-family: inherit;
+    color: inherit;
+    border: none;
+    cursor: pointer;
+    padding: 0.5em 1em;
+    background-color: #f1d190;
+    transition: background-color 0.15s;
+
+    &:hover,
+    &:active,
+    &:focus {
+        background-color: #ffffff;
+    }
 `;
 
 function BackgammonStartingRoll({ player, color, startingRolls }) {
