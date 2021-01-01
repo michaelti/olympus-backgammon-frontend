30956afe15fb2f1dc0c517a73c328093e705f56d
Michael <m@michaelti.ca>
2021-01-01 13:20:03 -0500

    Refactor button into own component

--- src/components/BoardUI/BackgammonStartingRoll.js
+++ src/components/BoardUI/BackgammonStartingRoll.js
@@ -2,28 +2,13 @@ import React from "react";
 import Die from "./Die";
 import { socketEmit } from "../../api";
 import styled from "styled-components";
+import Button from "./Button";
 
 const Container = styled.div`
     height: 100%;
     display: flex;
     justify-content: center;
     padding: 5px 0;
-`;
-
-const Button = styled.button`
-    font-family: inherit;
-    color: inherit;
-    border: none;
-    cursor: pointer;
-    padding: 0.5em 1em;
-    background-color: #f1d190;
-    transition: background-color 0.15s;
-
-    &:hover,
-    &:active,
-    &:focus {
-        background-color: #ffffff;
-    }
 `;
 
 function BackgammonStartingRoll({ player, color, startingRolls }) {
--- src/components/BoardUI/BoardButtons.js
+++ src/components/BoardUI/BoardButtons.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Button from "./Button";
 
 const Buttons = styled.div`
     height: 100%;
@@ -12,65 +13,26 @@ const Buttons = styled.div`
     gap: 5px;
 `;
 
-const Button = styled.button`
-    font-family: inherit;
+const BoardButton = styled(Button)`
     font-weight: bold;
-    color: inherit;
-    border: none;
-    cursor: pointer;
-    background-color: ${(props) => (props.highlight ? "#ffffff" : "#f1d190")};
     padding: 0 0.5em;
-    transition: background-color 0.15s;
-    position: relative;
 
-    &:hover,
-    &:active,
-    &:focus {
-        background-color: #ffffff;
-
-        ::after {
-            opacity: 1;
-        }
-    }
-
-    &:disabled {
-        background-color: #f1d190;
-        pointer-events: none;
-        opacity: 0.5;
-
-        ::after {
-            opacity: 0;
-        }
-    }
-
-    &::after {
-        content: attr(aria-label);
-        position: absolute;
-        bottom: calc(100% + 5px);
-        left: 50%;
-        transform: translateX(-50%);
-        pointer-events: none;
-        background-color: #212121;
-        color: #fff;
-        font-weight: normal;
-        font-size: 90%;
-        padding: 0 0.5em;
-        border-radius: 5px;
-        white-space: nowrap;
-        opacity: 0;
-        transition: opacity 0.15s;
-    }
+    ${(props) =>
+        props.highlight &&
+        css`
+            background-color: #ffffff;
+        `}
 `;
 
 function BoardButtons({ applyTurn, undoMove, canUndo, shouldFinish }) {
     return (
         <Buttons>
-            <Button onClick={applyTurn} highlight={shouldFinish} aria-label="Finish turn">
+            <BoardButton onClick={applyTurn} highlight={shouldFinish} label="Finish turn">
                 &#10003;
-            </Button>
-            <Button onClick={undoMove} disabled={!canUndo} aria-label="Undo move">
+            </BoardButton>
+            <BoardButton onClick={undoMove} disabled={!canUndo} label="Undo move">
                 &#8634;
-            </Button>
+            </BoardButton>
         </Buttons>
     );
 }
