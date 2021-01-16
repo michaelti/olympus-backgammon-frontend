64272f5c12d7bd5a1e7d5e384d14932b4003b2a4
blaiseIT <blaise@live.ca>
2021-01-16 12:41:20 -0500

    Refactor loop version of getPossiblePips

--- src/components/Game.js
+++ src/components/Game.js
@@ -23,8 +23,8 @@ function Game({ player, roomStep, startingRolls, variant, boardState, score, roo
         const clamp = (to) => (to < 0 ? 0 : to > 25 ? 25 : to);
         let end;
         if (variant === Variant.fevga) {
-            end = start - die;
             if (boardState.turn === Player.white) {
+                end = start - die;
                 if (start >= 13 && end <= 12) end = 25;
                 else if (end < 1) end += 24;
             } else {
@@ -36,33 +36,32 @@ function Game({ player, roomStep, startingRolls, variant, boardState, score, roo
         return end;
     };
 
-    const getPossiblePips = (pos0) => {
+    const getPossiblePips = (start) => {
         let possiblePips = {};
         if (!boardState.dice[0]) return possiblePips;
-        let boardMutate = { ...boards[variant](), ...clone(boardState) };
-        let pos1, pos2;
-        let pos = [];
+        let boardCopy = { ...boards[variant](), ...clone(boardState) };
+        let pos = [start];
         const die = boardState.dice;
 
         // Two unique dice remaining
         if (die.length === 2 && die[0] !== die[1]) {
-            pos1 = getEndPip(pos0, die[1]);
-            if (boardMutate.isMoveValid(pos0, pos1)) {
-                possiblePips[pos1] = [pos1];
-                boardMutate.doMove(pos0, pos1);
-                pos2 = getEndPip(pos1, die[0]);
-                if (boardMutate.isMoveValid(pos1, pos2)) possiblePips[pos2] = [pos1, pos2];
+            pos[1] = getEndPip(pos[0], die[1]);
+            if (boardCopy.isMoveValid(pos[0], pos[1])) {
+                possiblePips[pos[1]] = [pos[1]];
+                boardCopy.doMove(pos[0], pos[1]);
+                pos[2] = getEndPip(pos[1], die[0]);
+                if (boardCopy.isMoveValid(pos[1], pos[2])) possiblePips[pos[2]] = [pos[1], pos[2]];
             }
         }
-
-        boardMutate = { ...boards[variant](), ...clone(boardState) };
-        pos[0] = pos0;
+        boardCopy = { ...boards[variant](), ...clone(boardState) };
+        // Portes need something like this somewhere:
+        // boardCopy.bar = { [Player.black]: boardCopy.pips[25], [Player.white]: boardCopy.pips[0] };
         for (let i = 1; i <= die.length; i++) {
             pos[i] = getEndPip(pos[i - 1], die[i - 1]);
-            if (boardMutate.isMoveValid(pos[i - 1], pos[i])) {
+            if (boardCopy.isMoveValid(pos[i - 1], pos[i])) {
                 possiblePips[pos[i]] = pos.slice(1, i + 1);
-                // Here we call doMove when we may not have to.
-                boardMutate.doMove(pos[i - 1], pos[i]);
+                // TODO Optimize: here we call doMove once more than we have to.
+                boardCopy.doMove(pos[i - 1], pos[i]);
             } else break;
         }
 
